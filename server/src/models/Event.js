e5a19e5a8c9fa9771b16152859c7a72e46bdb4b8
Karan <karangera025@gmail.com>
2026-03-24 13:34:39 +0530

    schema of events

---
+++ server/src/models/Event.js
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose';
+
+const eventSchema = new Schema(
+    {
+        title : {
+            type : String,
+            required: [true, 'Event title is required'],
+            trim : true,
+        },
+        description : {
+            type: String,
+            required: [true, 'Description is required'],
+        },
+        category : {
+            type : String,
+            required : true,
+            enum : ['cultural', 'technical', 'gaming', 'art'],
+        },
+        date : {
+            type : Date,
+            required : [true, 'Event date is required'],
+        },
+        time : {
+            type: String,
+            required: [true, 'Event time is required'],
+        },
+        venue : {
+            type: String,
+            required: [true, 'Venue is required'],
+        },
+        isPaid : {
+            type: Boolean,
+            default: false,
+        },
+        registrationFee : {
+            type: Number,
+            default: 0,
+        },
+        teamSizeMin : {
+            type: Number,
+            default: 1,
+        },
+        teamSizeMax : {
+            type: Number,
+            default: 1,
+        },
+        totalSlots : {
+            type: Number,
+            required: [true, 'Total slots required'],
+        },
+        registeredCount : {
+            type: Number,
+            default: 0,
+        },
+        prizes : {
+            first : {type : String},
+            second : {type : String},
+            third : {type : String}
+        },
+        rules : [String],
+        poster : {
+            type : String,
+        },
+        createdBy : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        isActive: {
+            type : Boolean,
+            requited : true,
+        },
+        },
+        {timestamp : true},
+);
+
+const Event =  mongoose.model('Event' , eventSchema);
+export default Event;
