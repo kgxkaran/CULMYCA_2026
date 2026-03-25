b6a98a7c24118c0606f9a7219ee150d9305e5463
Karan <karangera025@gmail.com>
2026-03-25 22:50:11 +0530

    event

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,3 +1,3 @@
 export default function LandingPage() {
-  return <div className="text-white text-center mt-20 text-2xl">Landing Page 🎭</div>
+  return <div className="text-black text-center mt-20 text-2xl">Landing Page 🎭</div>
 }
--- client/src/pages/events pages/EventsPage.jsx
+++ client/src/pages/events pages/EventsPage.jsx
@@ -1,3 +1,3 @@
 export default function EventsPage() {
-  return <div className="text-white text-center mt-20 text-2xl">Events Page 🎪</div>
+  return <div className="text-amber-500 text-center mt-20 text-2xl">Events Page 🎪</div>
 }
